681bfa9ecf510fbd60af726d0ea57d02a7a608c2
Samantha <samanthachin.dsp@gmail.com>
2023-06-06 17:35:44 -0700

    services history work

--- ghi/app/src/ServiceHistory.js
+++ ghi/app/src/ServiceHistory.js
@@ -1,70 +1,58 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-function ServiceHistory(props){
-    const [searchVin, setSearchVin] = useState('');
-    const [filteredAppointments, setFilteredAppointments] = useState([]);
+function ServiceHistory({appointments}){
+  const [searchVin, setSearchVin] = useState('');
 
-    if (!props.appointments || !Array.isArray(props.appointments)){
-        return null;
-    }
+  const filteredAppointments = appointments.filter(
+    appointment => appointment.vin.includes(searchVin))
 
-    // UPDATE WITH BUTTON FUNCTIONALITY WITH CANCEL AND FINISH STATUS
-    // UPDATE WITH VIP STATUS
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const value = event.target.value
+    setSearchVin(value);
+  }
 
-    const handleSearch = (e) =>{
-        e.preventDefault();
-    }
-        const vinAppointments = props.appointments.filter((appointment) =>
-        appointment.vin.toLowerCase().includes(searchVin.toLowerCase())
-        );
-
-        setFilteredAppointments(vinAppointments);
-
-
-    return (
-      <>
-        <h1>Service History</h1>
-        <div className="search-bar-container">
-            <form onSubmit={handleSearch}>
-                <input
-                type="text"
-                placeholder="Search by VIN"
-                value={searchVin}
-                name="vin_search" id="vin_search"
-                onChange={(event) => {setSearchVin(event.target.value)}}/>
-                <button type="submit">Search</button>
-            </form>
-        </div>
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th>VIN</th>
-              <th>Is VIP?</th>
-              <th>Customer</th>
-              <th>Date & Time</th>
-              <th>Technician</th>
-              <th>Reason</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.appointments.map((appointment) => {
-              return (
-                <tr key={appointment.id}>
-                  <td>{appointment.vin}</td>
-                  <td>Placeholder for VIP</td>
-                  <td>{appointment.customer}</td>
-                  <td>{appointment.date_time}</td>
-                  <td>{appointment.technician}</td>
-                  <td>{appointment.reason}</td>
-                  <td>{appointment.status}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
+  return (
+    <>
+      <h1>Service History</h1>
+      <form onSubmit={handleSearch}>
+      <input
+        placeholder="Search by VIN..."
+        value={searchVin}
+        onChange={handleSearch}
+      />
+      <button type="submit" className="btn-primary btn-search">Search</button>
+      </form>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>VIN</th>
+            <th>Is VIP?</th>
+            <th>Customer</th>
+            <th>Date & Time</th>
+            <th>Technician</th>
+            <th>Reason</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredAppointments?.map((appointment) => {
+            return (
+              <tr key={appointment.id}>
+                <td>{appointment.vin}</td>
+                <td>{appointment.vip_status ? "Yes":"No"}</td>
+                <td>{appointment.customer}</td>
+                <td>{appointment.date_time}</td>
+                <td>{`${appointment.technician.first_name} ${appointment.technician.last_name}`}</td>
+                <td>{appointment.reason}</td>
+                <td>{appointment.status}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default ServiceHistory
